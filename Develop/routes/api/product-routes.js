e6cd4e0242f555447d23b6cb7bd93ab594e4a406
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-28 21:11:47 -0600

    the thing

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const pro = await Product.findAll({
+      // include: [{model: Tag, through: ProductTag, as: 'product_tag'}],
+      // {model: Category, as: 'product_category'},
+    });
+    res.status(200).json(pro);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const pro = await Product.findByPk(req.params.id, {
+      // include: [{model: Category}]
+    });
+    if (!pro) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(pro)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
